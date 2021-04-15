15c69ff2a1df10a490cb3450c6323a699060cbd0
Touzard Nicolas <78793616+NTO59@users.noreply.github.com>
2021-04-15 11:15:54 +0200

    layouts with Blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,32 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel</title>
-</head>
-<body>
-    <h1>Hello Laravel</h1>
+{{-- On va étendre le fichier resourses/views/layouts/app.blade.php --}}
+
+@extends('layouts.app') {{-- layout/app --}}
+
+{{-- On met le contenu suivant dans le yield content --}}
+@section('content')
+
+    <h1>Hello {{ $name }} </h1>
+
+    <ul>
+        @foreach ($bibis as  $bibi)
+            <li>
+                {{-- @dump($loop) --}}
+                 {{ $loop->index }} {{ $bibi }} 
+            </li>
+        @endforeach
+    </ul>
+
 
     <h2>Blade simplifie le PHP</h2>
     
-    <?php echo date('d/m/Y'); //en PHP ?>
+    <?php echo date('d/m/Y');?>
     
     {{ date('d/m/Y') }} 
     
     <h2>if en Blade</h2>
+
     @if (1===1)
     je suis un if
     @endif
     
     <h2>boucle en Blade</h2>
-    @for ($i = 0; $i < $count; $i++)
+
+    @for ($i = 0; $i < $i; $i++)
     {{ $i }}
     @endfor
     
     <h2>Protection XSS en Blade</h2>
+
     {{'<script> alert("toto") </script>'}}
     {!! '<h1> Pas de protection XSS </h1>' !!}
 
-</body>
-</html>
+@endsection
